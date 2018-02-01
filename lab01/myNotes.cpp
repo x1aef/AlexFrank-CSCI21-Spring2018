e086ba77fd831198cf24d74f319bec87b6f69de4
Alex <afrank003@student.butte.edu>
2018-02-01 22:55:35 +0000

    project1.cpp

---
+++ lab01/myNotes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main ()
+{
+    long int num1 = 5467367467467;
+    long int num2 = 3453647356;
+    
+    
+    cout << num1 << " mod 10: " << num1%10 << endl;
+    cout << num2 << " mod 10: " << num1%10 << endl;
+    
+    cout << endl << endl;
+        
+    string str3 = "";
+    string str4 = "3353423";
+    str3.append(str4);
+        
+    cout << "String 3: " << str3;
+
+    return 0;
+}
